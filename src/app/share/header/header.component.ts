8a74ec1ecb2c3a476f639725f742522f83e14108
HoangCaoPhi <hoangcaophi11061999@gmail.com>
2020-07-23 17:13:19 -0700

    Them modal cap nhat thong tin user

--- src/app/share/header/header.component.ts
+++ src/app/share/header/header.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 import { AuthenticationService } from './../../services/authentication.service';
 import { User } from './../../models';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -16,7 +17,8 @@ export class HeaderComponent implements OnInit {
   
   constructor(
       private router: Router,
-      private authenticationService: AuthenticationService
+      private authenticationService: AuthenticationService,
+      private fb: FormBuilder,
   ) {
       this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
   }
@@ -25,8 +27,14 @@ export class HeaderComponent implements OnInit {
       this.authenticationService.logout();
       this.router.navigate(['/login']);
   }
-
+  form: FormGroup;
+  submitted = false;
   ngOnInit(): void {
+    this.form = this.fb.group({
+      firstname: ["", [Validators.required]],
+      lastname: ["", [Validators.required]],
+      username: ["", [Validators.required]],
+      phone: ["", [Validators.required, Validators.pattern(("(09|03|08)[0-9 ]{8}"))]],
+    })
   }
-
 }
